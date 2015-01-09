873d501e9d1521749bd67f9774ff808774a1956e
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-09 18:28:41 +0000

    Added play path

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -16,6 +16,9 @@ module NavigationHelpers
     when /name/
       'name'
 
+    when /play/
+      'name'
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
