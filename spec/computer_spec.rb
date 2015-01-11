20e0264dbf61b79e662eb73ff422e16a559102f8
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-11 19:33:28 +0000

    Adjusted test to test for rock, paper or scissors.

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,7 +1,11 @@
+require 'computer'
+
 describe Computer do 
 
+  let(:computer) { Computer.new }
+
   it 'should make a move' do 
-    expect(computer.move).to eq "rock" || "paper" || "scissors"
+    expect(computer.move).to satisfy { |move| ['rock', 'paper', 'scissors'].include?(move) }
   end
 
 end
