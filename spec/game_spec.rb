9c1e3473834b7c72681ad2f0655374886b06072c
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-11 20:15:52 +0000

    Made test pass.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,11 +6,14 @@ describe Game do
   let(:computer) { Computer.new }
   let(:game) { Game.new }
 
+  before do 
+    game.add_player(player)
+    game.add_player(comuter)
+  end
+
   it 'should know when a game has been won' do
     player.move = "rock"
     allow(computer).to receive(:move).and_return("scissors")
-    game.add_player(player)
-    game.add_player(player)
     expect(game.win?).to be true
   end
 
