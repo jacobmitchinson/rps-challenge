cc79b5724b6b5014b2d32f92b1a779e440bd7cff
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-11 23:05:59 +0000

    Added custom step to double computer and get tests to pass.

--- features/step_definitions/custom_steps.rb
+++ features/step_definitions/custom_steps.rb
@@ -1,5 +1,10 @@
+
 Given(/^I have set my name as "(.*?)"$/) do |name|
   visit path_to('/the homepage/')
   fill_in("name", :with => name)
   click_button("submit")
+end
+
+Given(/^the computer chooses "(.*?)"$/) do |move|
+  allow_any_instance_of(Computer).to receive(:move).and_return("scissors")
 end
